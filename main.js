3fe74af817afde3fdead38e702dc14a489cee9f6
Valentina Valenzuela <valevalenzuela6@gmail.com>
2017-04-07 14:15:59 -0300

    NoCumpleanos

---
+++ main.js
@@ -0,0 +1,12 @@
+var meses = ["enero", "febrero", "marzo", "abril", "mayo", "junio", "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"]
+
+var mesCumple = prompt ("ingresa el mes de tu cumpleaños")
+
+for (var i=0; i< meses.length;i++) {
+
+if (meses[i] != mesCumple) {
+{ console.log (meses[i])
+}
+}
+
+}
